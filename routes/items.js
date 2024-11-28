6b57e335c4bea89c26761a9431a86e914ad9d408
Jaymon <jaymon@gmail.com>
2024-11-29 02:05:55 +0800

    folders fix

--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const itemsController = require('../controllers/itemsController');
 
-// Define your routes here
-router.get('/', (req, res) => {
-    res.send('Items route working!');
-});
+router.get('/', itemsController.getAllItems);
+router.post('/', itemsController.createItem);
+router.put('/:id', itemsController.updateItem);
+router.patch('/:id', itemsController.patchItem);
+router.delete('/:id', itemsController.deleteItem);
 
 module.exports = router;
