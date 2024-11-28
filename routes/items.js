e2cdc36b881e959f659c805d0073292374c8912f
Jaymon <jaymon@gmail.com>
2024-11-29 02:58:39 +0800

    typo

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const itemsController = require('../controllers/itemsController');
+const itemsController = require('../controllers/itemsControllers');
 
 router.get('/', itemsController.getAllItems);
 router.post('/', itemsController.createItem);
